6fc2099df4cbef827252552514735b9615cde8a1
Mobasher Sobhan <moba2k@yahoo.com>
2018-06-08 15:57:38 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -3,28 +3,24 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+  var thisIsCrazy = function (){  // fix the code in here:
+    console.log("hey!!!")  // fix the code in here:
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
+  function sayMy() {
+    var name = "Kristin";
+    console.log(name);
+    }
+    sayMy();
   // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
 }
